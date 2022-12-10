2bfa6b05870d805aec90fae5c875719a2ae5ad00
GulGulHub <guelden.ediger@gmx.net>
2022-12-10 14:06:44 +0100

    finally made database work

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app.config['SECRET_KEY'] = '8bc5df3b90c6dfb32dcbdfcfb485cb35'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+app.app_context().push()
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    useremail = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
@@ -27,7 +29,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     def __repr__(self):
-        return f"Post('{self.titel}','{self.date_posted}')"
+        return f"Post('{self.title}','{self.date_posted}')"
 
 
 posts = [
